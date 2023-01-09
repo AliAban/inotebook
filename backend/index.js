306cfd17fb459ed6f0f03786d0c230c36594f810
AliAban <dlsabanshaikh@gmail.com>
2023-01-09 12:16:24 +0530

    created the NoteItem component

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,6 @@ app.use('/api/auth', require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`i-NoteBook backend listening on port ${port}`)
 })
 
